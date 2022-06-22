5296b29c2cfd7caedf9f1f3c568a5257c0c5e7cc
34j <55338215+34j@users.noreply.github.com>
2022-06-22 18:57:17 +0900

    プロジェクト ファイルを追加します。

---
+++ README.md
@@ -0,0 +1,18 @@
+﻿# SeagullTemplate.CLI
+## Usage
+For information on how to publish the application, please refer to [34j/dotnet-publish-multiplatform.sh](https://github.com/34j/dotnet-publish-multiplatform.sh).
+
+# $safeprojectname$
+## Requirements
+win-x64, win-x86, win-arm, win-arm64, osx-x64, linux-x64, linux-arm
+
+## Installation
+1. Download the latest release from [Releases](https://github.com/[username]/$safeprojectname$/releases).
+2. Add the folder where the program is located to the environment variables.
+
+## Description
+
+## Usage
+
+
+
---
+++ SeagullTemplateCLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeagullTemplateCLI
+{
+    class Program
+    {
+        static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Argument(0, Description = "argument"), Required]
+        public string Argument0 { get; private set; } = "";
+        [Option(ShortName = "o", LongName = "option", Description = "option")]
+        public string Option0 { get; private set; } = "";
+        private void OnExecute()
+        {
+            ;
+        }
+    }
+}
